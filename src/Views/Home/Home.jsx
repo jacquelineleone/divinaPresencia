ac88f154d7361539192bf75f70234326d5428c63
jacquelineleone <jacqueline.leone02@gmail.com>
2024-03-21 20:44:28 -0300

    Home - web view

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -3,8 +3,11 @@ import style from "./styles/home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
+    <div className={style.container}>
+      <div className={style.title}>
+        <h1>La divina presencia</h1>
+        <p>La mejor calidad, en un solo lugar</p>
+      </div>
     </div>
   );
 }
