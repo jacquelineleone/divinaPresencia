b2345d0d92653d59ff0e840b5b13230cd83fb9ab
jacquelineleone <jacqueline.leone02@gmail.com>
2024-03-21 20:53:26 -0300

    Modularized home

---
+++ src/Views/Home/sections/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "../styles/header.module.css";
+
+export default function Header() {
+  return (
+    <div className={style.container}>
+      <div className={style.title}>
+        <h1>La divina presencia</h1>
+        <p>La mejor calidad, en un solo lugar</p>
+      </div>
+    </div>
+  );
+}
