a51c852393ade2da0ac08a6f29c4b1804c35b36b
jacquelineleone <jacqueline.leone02@gmail.com>
2024-03-24 23:32:16 -0300

    'Destacados' section -

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import style from "./styles/home.module.css";
+// Sections
 import Header from "./sections/Header";
+import Destacados from "./sections/Destacados";
 
 export default function Home() {
   return (
-    <>
+    <div className={style.container}>
       <Header />
-    </>
+      <Destacados />
+    </div>
   );
 }
--- src/Views/Home/sections/Destacados.jsx
+++ src/Views/Home/sections/Destacados.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import style from "../styles/destacados.module.css";
+import productos from "../json/destacados.json";
+import imagen from "../../../Assets/Images/Home/Producto01.png";
+
+const Product = ({ image, name, description }) => {
+  return (
+    <div className={style.productContainer}>
+      <img src={imagen} alt="" />
+      <div className={style.content}>
+        <h6>{name}</h6>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default function Destacados() {
   return (
-    <div>
-      <p>Destacados</p>
+    <div className={style.container}>
+      <h4>Productos Destacados</h4>
+      <div className={style.products}>
+        {productos.map((producto) => (
+          <Product
+            image={producto.image}
+            name={producto.name}
+            description={producto.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
