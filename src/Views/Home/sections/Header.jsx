6a1f1eb7da1fa049d65ad7e9fc8e1a61f4911cdd
jacquelineleone <jacqueline.leone02@gmail.com>
2024-04-01 13:40:22 -0300

    Dot added in caption

--- src/Views/Home/sections/Header.jsx
+++ src/Views/Home/sections/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className={style.container}>
       <div className={style.title}>
         <h1>La divina presencia</h1>
-        <p>La mejor calidad, en un solo lugar</p>
+        <p>La mejor calidad, en un solo lugar.</p>
       </div>
     </div>
   );
