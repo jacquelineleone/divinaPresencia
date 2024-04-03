62fcd8e36a82f7cd2a25d22cbbbbd96a408dc074
jacquelineleone <jacqueline.leone02@gmail.com>
2024-04-02 21:47:18 -0300

    Detail component - params access

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Rutas from "./Routes";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const isProductPage = location.pathname.startsWith("/producto/");
+
   return (
     <div className="general-container">
-      <Navbar />
+      {!isProductPage && <Navbar />}
       <Rutas />
       <Footer />
     </div>
